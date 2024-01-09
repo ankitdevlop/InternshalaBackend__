aa83779b96d575e9fdae281b707ad181c01b645e
Ankit Dubey <106301985+ankitdevlop@users.noreply.github.com>
2024-01-09 23:24:40 +0530

    changes

--- Routers/ApplicationRouter.js
+++ Routers/ApplicationRouter.js
@@ -10,8 +10,7 @@ router.post('/',async(req,res)=>{
         user:req.body.user,
         company:req.body.company,
         category:req.body.category,
-  
-        ApplicationId:req.body.ApplicationId
+   ApplicationId:req.body.ApplicationId
     })
     await applicationData.save().then((data)=>{
 res.send(data)
@@ -29,5 +28,24 @@ router.get('/', async (req, res) => {
       res.status(500).json({ error: 'Internal Server Error' });
     }
   });
+
+  router.get('/:id', async (req, res) => {
+    const { id } = req.params;
+  
+    try {
+      // Fetch a document by ID from the application collection
+      const application = await application.findById(id);
+  
+      if (!application) {
+        return res.status(404).json({ success: false, error: 'application not found' });
+      }
+  
+      res.status(200).json({ success: true, data: application });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ success: false, error: 'Internal Server Error' });
+    }
+  });
+  
 module.exports=router
 
